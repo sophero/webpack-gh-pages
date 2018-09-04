4cd7c05fe438486f51992313ff1d56329cc9cd0a
Sophia Di <backwardsxmarathon@gmail.com>
2018-09-04 18:32:46 -0400

    Initial commit

--- src/router.js
+++ src/router.js
@@ -3,20 +3,41 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+// making routes js object is what react-router does with jsx-defined routes
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cb(null, module.default));
+      }
+    }
+    // Note that we don't split the System.import() call off into a helper function here, since webpack is going to scan our code and at each System.import() call create a module out of the component's literal path string - according to Steven webpack is not equipped to automatically interpolate, concatenate here.
+  ]
+};
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'redux', 'react', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk'
+]
 
 module.exports = {
-  entry: './src/index.js',
+  entry: { // listing multiple key-value pairs here means we will have multiple builds each specifying its own entry point
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js' // [name] is injected with relevant key name specified in entry section. [chunkhash] created on build, unique id/hash based on build output.
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [ // plugins operate on bundled outputs, whereas loaders operate on application files
+    new webpack.optimize.CommonsChunkPlugin({
+      // name: 'vendor' // this will look for crossover files between 'vendor' build and default 'bundle' build, and include these only in 'vendor' output. nice!
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({ // adds script tags into optionally user specified html template for all scripts outputted by webpack. script filenames include chunkhash as included in output section.
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
